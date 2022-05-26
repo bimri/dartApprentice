001653f477cf2ccc51339b0fb73fc6fd7e31ddbc
bimri <bimri@outlook.com>
2022-05-26 23:40:24 +0300

    ternary ioperator

---
+++ chap4/ternary.dart
@@ -0,0 +1,29 @@
+// The ternary conditional operator
+
+const score = 83;
+
+void main() {
+  String message;
+  if (score >= 60) {
+    message = 'You passed';
+  } else {
+    message = 'You failed';
+  }
+
+
+const score1 = 83;
+
+const message1 = (score >= 60) ? 'You Passed!' : "You've Failed.";
+}
+
+/* 
+
+The ternary conditional operator takes a condition and
+returns one of two values, depending on whether the
+condition is true or false. 
+
+(condition) ? valueIfTrue : valueIfFalse;
+
+*/
+
+
