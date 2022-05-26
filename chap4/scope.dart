e8d80ccc67922d781187cba309c429b974a8b1d8
bimri <bimri@outlook.com>
2022-05-26 23:01:12 +0300

    Variable scope

---
+++ chap4/scope.dart
@@ -0,0 +1,17 @@
+const global = 'Hello, World';
+
+void main() {
+  const local = 'Hello, main';
+
+  if (2 > 1) {
+    const insideIf = 'Hello, anybody?';
+
+    print(global);
+    print(local);
+    print(insideIf);
+  }
+
+  print(global);
+  print(local);
+  // print(insideIf);     // NOT ALLOWED!
+}
